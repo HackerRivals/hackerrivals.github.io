19d85d82b06db137964f06f9a3867423449d993f
Jaskaran <thecodersingh@gmail.com>
2026-01-20 16:26:24 -0800

    Update venue

--- src/constants/eventConfig.ts
+++ src/constants/eventConfig.ts
@@ -29,7 +29,7 @@ export const eventConfig = {
     event: {
       date: 'May 1, 2026',
       location: 'Science World • 10am-5pm',
-      afterParty: '+ VIP After Party @ AWS Building',
+      afterParty: '+ VIP After Party @ Science World',
       prize: '$1000 Cash Prize',
     },
     countdown: {
